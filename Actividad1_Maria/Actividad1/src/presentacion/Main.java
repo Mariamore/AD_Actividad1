91a3859d9f933b7e45b4d134f855f7c921467ba4
maria.moreno <maria.moreno.r96@gmail.com>
2024-01-27 19:10:28 +0100

    Cambios version maria

--- Actividad1_Maria/Actividad1/src/presentacion/Main.java
+++ Actividad1_Maria/Actividad1/src/presentacion/Main.java
@@ -1,4 +1,4 @@
-package controlador;
+package presentacion;
 
 import java.io.DataOutputStream;
 import java.io.File;
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 import modelo.entidad.Articulo;
